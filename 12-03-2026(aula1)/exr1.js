9ed4992a3614f03a7e7c38bd73704f2b59559e5f
tobi-anfitras <gustavomiara1996@gmail.com>
2026-03-12 16:02:27 -0300

    exercicio_basico

---
+++ 12-03-2026(aula1)/exr1.js
@@ -0,0 +1,43 @@
+//adição
+
+let num1 = 7
+let num2 = 8
+
+console.log(num1 + num2)
+ 
+//subtração
+
+let num3 = 7
+let num4 = 8
+
+
+console.log(num1-num2)
+
+//multiplicação
+
+let num5 = 7
+let num6 = 8
+
+console.log(num1 * num2)
+
+//divição
+
+let num7 = 7
+let num8 = 8
+
+console.log(num1 / num2)
+
+//resto
+
+let num9 = 19
+let num10 = 5
+
+console.log(num1 % num2)
+
+//potenciação
+
+let num11 = 2
+let num12 = 7
+
+let res = num1**num2
+console.log(res)
