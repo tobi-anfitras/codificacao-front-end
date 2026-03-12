cdfd79ce84704e4f8ce0198c907a890df31dcb0a
tobi-anfitras <gustavomiara1996@gmail.com>
2026-03-12 16:54:16 -0300

    Create exr2.js

---
+++ 12-03-2026(aula1)/exr2.js
@@ -0,0 +1,10 @@
+let idade = 61
+let preco = 150
+
+if(idade>60 && preco>100){
+ let precoFinal =preco - (preco * 0.2);
+
+    console.log("parabens voce teve um disconto de 20% o preço final e :"+precoFinal)
+}else{
+    console.log("sem desconto o preço total e de:"+preco)
+}
