bee58cd8c7323e6d135566f36a10aac1342220dc
tobi-anfitras <gustavomiara1996@gmail.com>
2026-03-19 17:13:32 -0300

    Create pjAcoes.js

---
+++ 19_03_2026/pjAcoes.js
@@ -0,0 +1,21 @@
+export function acordar(){
+    let acordar = "acorda vagabundo"
+console.log(acordar)
+}
+export function levantar(){
+let levantar = "levantando;"
+console.log(levantar)
+
+}
+export function comer(){
+let comer = "recaregando o hp"
+console.log(comer);
+
+}
+export function dormir(){
+let dormir = "dormindo"
+console.log(dormir);
+
+
+}
+
